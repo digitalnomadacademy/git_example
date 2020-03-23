974d2259502a81dcc2eb655468f851567e1eb4b6
Matej The Tree <matejthetree@gmail.com>
2020-03-23 19:44:09 +0100

    example container

---
+++ lib/container_example.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ContainerExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 100,
+      color: Colors.greenAccent,
+    );
+  }
+}
