c02cd1a3406512a0c29ecda47b046cb9a4429f1f
Matej The Tree <matejthetree@gmail.com>
2020-03-23 19:47:45 +0100

    padding

--- lib/container_example.dart
+++ lib/container_example.dart
@@ -4,6 +4,7 @@ class ContainerExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(8),
       height: 100,
       width: 100,
       color: Colors.greenAccent,
